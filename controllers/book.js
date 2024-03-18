b3ab188d37bb05245cbfe86602f2a227d51ecf62
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-18 09:45:03 +0100

    Add modifyBook function

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,5 @@
-const Book = require('../models/Book')
+const Book = require('../models/Book');
+const path = require('path');
 const fs = require('fs');
 
 // READ
@@ -48,6 +49,37 @@ exports.getAllBooks = (req, res, next) => {
     book.save()
     .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
+  };
+  
+  // MODIFY
+
+  exports.modifyBook = (req, res, next) => {
+    const bookObject = req.file ? {
+      ...JSON.parse(req.body.book),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+    .then((book) => {
+      if (book.userId != req.auth.userId) {
+        res.status(401).json({ message: 'Not allowed'});
+      } else {
+        if (req.file && book.imageUrl) {
+          const imagePath = path.join(__dirname, '..', 'images', path.basename(book.imageUrl));
+          // Check if the file exists, then delete it
+          if (fs.existsSync(imagePath)) {
+            fs.unlinkSync(imagePath);
+          }
+        }
+        Book.updateOne({_id: req.params.id}, {...bookObject, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Object modified !'}))
+        .catch(error => res.status(401).json({ error }))
+      }
+    })
+    .catch((error) =>{
+      res.status(400).json({ error });
+    })
   };
 
   // DELETE
