175bc77005fe438752d20da45e118c1826242853
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-18 09:44:38 +0100

    Add multer to path and future new route

--- routes/book.js
+++ routes/book.js
@@ -9,7 +9,8 @@ const bookCtrl = require('../controllers/book');
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-// router.put('/:id', auth, bookCtrl.modifyBook);
+//router.post('/:id/rating/', auth, bookCtrl.addRating);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
