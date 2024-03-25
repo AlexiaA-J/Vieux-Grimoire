a4ef375e7cbc0d5ba917f04dc3275f4e2e5ec165
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-25 08:43:32 +0100

    Add rating related routes

--- routes/book.js
+++ routes/book.js
@@ -9,7 +9,8 @@ const bookCtrl = require('../controllers/book');
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
 router.get('/:id', bookCtrl.getOneBook);
-//router.post('/:id/rating/', auth, bookCtrl.addRating);
+router.post('/:id/rating/', auth, bookCtrl.addRating);
+router.get('/bestrating', bookCtrl.bestratingBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
