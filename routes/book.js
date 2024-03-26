3e67a0184029bea6bbe8b106e17088762ddf2af3
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-26 08:36:30 +0100

    Fix bestratingBook route position

--- routes/book.js
+++ routes/book.js
@@ -8,9 +8,9 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
+router.get('/bestrating', bookCtrl.bestratingBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.post('/:id/rating/', auth, bookCtrl.addRating);
-router.get('/bestrating', bookCtrl.bestratingBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
