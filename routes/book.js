b797699f3c03ee9bb614b92b95e6a0d61223a95e
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-03-27 10:49:47 +0100

    Add sharp config to routes

--- routes/book.js
+++ routes/book.js
@@ -5,13 +5,15 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const bookCtrl = require('../controllers/book');
+const compressImage = require('../middleware/sharp-config');
+
 
 router.get('/', bookCtrl.getAllBooks);
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, multer, compressImage, bookCtrl.createBook);
 router.get('/bestrating', bookCtrl.bestratingBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.post('/:id/rating/', auth, bookCtrl.addRating);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, compressImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
